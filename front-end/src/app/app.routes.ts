cfaa703a46f9b6ead9b0781ed4caba22b9097f37
Hélio Filho <helioosilvafilho@gmail.com>
2026-03-19 10:32:45 -0300

    feat: add HTTP client provider and user creation route to application configuration

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
+import { CreateUserComponent } from './pages/users/create-user/create-user.component';
 import { LoginComponent } from './pages/login/login.component';
 import { UsersComponent } from './pages/users/users.component';
 
@@ -7,5 +8,6 @@ export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'users', component: UsersComponent, canActivate: [authGuard] },
+  { path: 'users/create', component: CreateUserComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: '/login' },
 ];
